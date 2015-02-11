eabf04b1004fc8d5f01aeaf19329f1d302bd7dd9
Brian Capouch <brianc@saintjoe.edu>
2015-02-11 01:27:33 -0500

    Use all the parts of the Body class in the driver

--- BodyDriver.java
+++ BodyDriver.java
@@ -1,8 +1,14 @@
 public class BodyDriver {
   public static void main(String[] args) {
 	Body sun = new Body();
-	sun.idNum = Body.nextID++;
+	// sun.idNum = Body.nextID++;
 	sun.name = "Sol";
 	sun.orbits = null; // Sun don't orbit nobody
+	System.out.println(sun.toString());
+
+	Body earth = new Body("Earth", sun);
+	// earth.idNum = Body.nextID++;
+	earth.orbits = sun;
+	System.out.println(earth.toString());
 	}
 }
